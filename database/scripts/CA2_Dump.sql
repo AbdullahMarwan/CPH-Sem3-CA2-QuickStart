45982aa8d9b1c866a047c06c7e01be0b70453478
AbdullahMarwan <abdu.moussa01@gmail.com>
2023-04-23 21:29:54 +0200

    Added correct CA2 Database script

--- database/scripts/CA2_Dump.sql
+++ database/scripts/CA2_Dump.sql
@@ -1,8 +1,37 @@
-CREATE DATABASE  IF NOT EXISTS `startcode` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `startcode`;
+CREATE DATABASE  IF NOT EXISTS `ca2_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ca2_test`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: localhost    Database: startcode
+-- Host: localhost    Database: ca2_test
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-22 15:25:58
+CREATE DATABASE  IF NOT EXISTS `ca2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ca2`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ca2
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -38,6 +67,30 @@ LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
 INSERT INTO `roles` VALUES ('admin'),('user');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_facts`
+--
+
+DROP TABLE IF EXISTS `user_facts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_facts` (
+  `user_name` varchar(25) NOT NULL,
+  `fact` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_name`),
+  CONSTRAINT `fk_facts_users1` FOREIGN KEY (`user_name`) REFERENCES `users` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_facts`
+--
+
+LOCK TABLES `user_facts` WRITE;
+/*!40000 ALTER TABLE `user_facts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_facts` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -90,10 +143,6 @@ LOCK TABLES `users` WRITE;
 INSERT INTO `users` VALUES ('admin','$2a$10$x/T8u1Ukfg2TsqFC4yKbDeyMIlaCBPDo1uHeZ4bKjBMYLIQjxDJnS'),('user','$2a$10$906O/KzQjjYjCzvDjxChZO1p3UdNbBylcc04ymLaZzwVsS9VWmNJe'),('user_admin','$2a$10$mQUCcUKV3jNqaBIiY6Ju6O09CY0QU5whbbZyfHrI1sA3m92e53/JS');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'startcode'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -104,4 +153,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-18 10:30:01
+-- Dump completed on 2023-04-22 15:25:59
